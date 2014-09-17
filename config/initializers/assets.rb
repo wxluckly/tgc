4a55053c25866509cfe8642d7e4631076bd60d2e
Joey wang <wxluckly@gmail.com>
2014-09-17 22:46:12 +0800

    coding

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(reset.css app_bootstrap.css bootstrap-ie6.css ie.css admin.css)
-Rails.application.config.assets.precompile += %w(admin.js )
+Rails.application.config.assets.precompile += %w(admin.js DD_belatedPNG_0.0.8a-min.js)
 Rails.application.config.assets.precompile += %w( twitter/glyphicons-halflings-white.png )
