0776d50d821aaa4daa3fb48089fa96870454d065
Joey wang <wxluckly@gmail.com>
2014-09-20 20:35:25 +0800

    coding

--- app/mailers/mailer_system.rb
+++ app/mailers/mailer_system.rb
@@ -2,9 +2,9 @@ class MailerSystem < ActionMailer::Base
   default from: 'thinkgrand_poster@163.com'
 
   def user_contact_notify(contact_id)
-    email = "44289889@qq.com"
+    email = "info@thinkgrand.com.cn"
     @contact = Contact.find(contact_id)
-    mail(to: email, subject: '有用户在网站提交了沟通信息')
+    mail(to: email, subject: '【胜观传播】网站通知')
   end
 
 end
